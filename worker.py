58aa6d98574cb599219cb375fb20e278f1ca44e9
Thomas Butler <10285945+thomaseb191@users.noreply.github.com>
2022-04-11 16:10:42 -0400

    Memo-ization

--- worker.py
+++ worker.py
@@ -1,11 +1,25 @@
 import wordle
 
-def evCalc(guess, answerList):
-    divisor = len(answerList)
+def evCalc(guess, answerList, aL):
     ev = 0
-    for solution in answerList:
-        response = wordle.score(solution, guess)
-        remaining = wordle.filter_words(answerList, guess, response)
-        ev += len(remaining)/divisor
-    print(guess + " " + str(ev))
+    mem = {}
+    for x,y in answerList:
+        divisor = len(answerList)
+        xRes = wordle.score(x, guess)
+        yRes = wordle.score(y, guess)
+        xResTup = tuple(xRes)
+        if xResTup in mem.keys():
+            ev += mem[xResTup]
+        else:
+            number = len(wordle.filter_words(aL, guess, xRes))/divisor
+            ev += number
+            mem[xResTup] = number
+        yResTup = tuple(yRes)
+        if yResTup in mem.keys():
+            ev += mem[yResTup]
+        else:
+            number = len(wordle.filter_words(aL, guess, yRes))/divisor
+            ev += number
+            mem[yResTup] = number
+    #print(guess + " " + str(ev))
     return ev
