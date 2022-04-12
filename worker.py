49b735d24ee855df32e6028245ae23907e6a4695
Thomas Butler <10285945+thomaseb191@users.noreply.github.com>
2022-04-12 12:40:51 -0400

    Fix analysis

--- worker.py
+++ worker.py
@@ -7,19 +7,24 @@ def evCalc(guess, answerList, aL):
         divisor = len(answerList)
         xRes = wordle.score(x, guess)
         yRes = wordle.score(y, guess)
+        
+        val = 1/divisor
+        
         xResTup = tuple(xRes)
         if xResTup in mem.keys():
-            ev += mem[xResTup]
+            val *= mem[xResTup]
         else:
-            number = len(wordle.filter_words(aL, guess, xRes))/divisor
-            ev += number
+            number = len(wordle.filter_words(aL, guess, xRes))
+            val *= number
             mem[xResTup] = number
         yResTup = tuple(yRes)
         if yResTup in mem.keys():
-            ev += mem[yResTup]
+            val *= mem[yResTup]
         else:
-            number = len(wordle.filter_words(aL, guess, yRes))/divisor
-            ev += number
+            number = len(wordle.filter_words(aL, guess, yRes))
+            val *= number
             mem[yResTup] = number
+            
+        ev += val
     #print(guess + " " + str(ev))
     return ev
